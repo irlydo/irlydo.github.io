0a67575dbd50acc60e2992f3fbe43d8ed2ada0a1
fokum8 <1213452745e75b>
2015-04-26 23:41:33 +0100

    1

---
+++ etc/writer/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE HTML>
+<html>
+    <head>
+        <title>Swrite</title>
+        <link type="text/css" href="css/style.css" rel="stylesheet">
+        <script src="localhost/js/jquery.min.js"></script>
+		<script src="localhost/js/jquery.scrolly.min.js"></script>
+		<script src="localhost/js/jquery.poptrox.min.js"></script>
+		<script src="localhost/js/skel.min.js"></script>
+		<script src="localhost/js/init.js"></script>
+    </head>
+    
+    <body>
+        <div class="header">
+            <input class="search"></input>
+        </div>
+    </body>
+</html>
